343a17bb84d0b3236b2ef0e321ab15f03930c2a3
DucMinh <minhmomang69@gmail.com>
2022-02-10 21:04:11 +0700

    Add AccountBean and TeacherBean

--- src/java/Bean/AccountBean.java
+++ src/java/Bean/AccountBean.java
@@ -1,9 +1,157 @@
 package Bean;
 
+import java.sql.Date;
+
 /**
- *
- * @author win
+ * Document: Java bean for Account object
+ * Created on: Feb 10, 2022, 8:20:03 PM
+ * @author Duc Minh
  */
 public class AccountBean {
+    private String username;
+    private String password;
+    private String mail;
+    private String avatar;
+    private String displayName;
+    private Date dateOfBirth;
+    private boolean sex;
+    private String description;
+    private int cash;
+    private Date createDate;
+    private String role;
+    private String status;
+    private boolean state;
+
+    public AccountBean() {
+    }
+
+    public AccountBean(String username, String password, String mail, String avatar, String displayName, Date dateOfBirth, boolean sex, String description, int cash, Date createDate, String role, String status, boolean state) {
+        this.username = username;
+        this.password = password;
+        this.mail = mail;
+        this.avatar = avatar;
+        this.displayName = displayName;
+        this.dateOfBirth = dateOfBirth;
+        this.sex = sex;
+        this.description = description;
+        this.cash = cash;
+        this.createDate = createDate;
+        this.role = role;
+        this.status = status;
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountBean{" + "username=" + username + ", password=" + password + ", mail=" + mail + ", avatar=" + avatar + ", displayName=" + displayName + ", dateOfBirth=" + dateOfBirth + ", sex=" + sex + ", description=" + description + ", cash=" + cash + ", createDate=" + createDate + ", role=" + role + ", status=" + status + ", state=" + state + '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+    
+    
+    
+    
     
 }
--- src/java/Bean/TeacherBean.java
+++ src/java/Bean/TeacherBean.java
@@ -1,9 +1,50 @@
 package Bean;
 
+import java.sql.Date;
+
 /**
- *
- * @author win
+ * Document: Java bean for Teacher's Account object
+ * Created on: Feb 10, 2022, 8:25:03 PM
+ * @author Duc Minh
  */
-public class TeacherBean {
+public class TeacherBean extends AccountBean{
+    private String cvImg;
+    private int subjectId;
+
+    public TeacherBean() {
+    }
+
+    public TeacherBean(String cvImg, int subjectId) {
+        this.cvImg = cvImg;
+        this.subjectId = subjectId;
+    }
+
+    public TeacherBean(String cvImg, int subjectId, String username, String password, String mail, String avatar, String displayName, Date dateOfBirth, boolean sex, String description, int cash, Date createDate, String role, String status, boolean state) {
+        super(username, password, mail, avatar, displayName, dateOfBirth, sex, description, cash, createDate, role, status, state);
+        this.cvImg = cvImg;
+        this.subjectId = subjectId;
+    }
+
+    @Override
+    public String toString() {
+        return "TeacherBean{" + "cvImg=" + cvImg + ", subjectId=" + subjectId + '}';
+    }
+
+    public String getCvImg() {
+        return cvImg;
+    }
+
+    public void setCvImg(String cvImg) {
+        this.cvImg = cvImg;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+    
     
 }
